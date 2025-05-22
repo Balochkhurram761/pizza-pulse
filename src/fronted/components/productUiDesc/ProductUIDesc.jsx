726902595a9817f998d58525fe6346a6f02882d5
balochkurram761 <balochkhurram102@gmail.com>
2025-05-22 03:56:02 -0700

    productUi Desc add some logics

--- src/fronted/components/productUiDesc/ProductUIDesc.jsx
+++ src/fronted/components/productUiDesc/ProductUIDesc.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import Dialog from '@mui/material/Dialog';
 import styles from '../../styles/productUiDesc/ProductUiDesc.module.css'
 import { IoMdClose } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import { style } from '@mui/system';
+
 const ProductUIDesc = ({ Isopen, close, pizza }) => {
+ const [selections, setSelections] = useState({
+    size: "small",
+    crust: "",
+    optional: [],
+    instructions: false,
+  });
+
+  const handleToggle = (key, value) => {
+   setSelections((prev)=>{
+    if(key==="optional"){
+      const handleOptional=prev.optional.includes(value) ?
+      prev.optional.filter((item)=>item!==value):
+      [...prev.optional , value ]
+      return  {...prev, optional:handleOptional}
+    }
+   return  {...prev , [key]:value}
+   })
+  
+  };
+
   if (!pizza) return null;
+  
   return (
     <Dialog open={Isopen} onClose={close} PaperProps={{
       style: {
@@ -33,15 +57,16 @@ const ProductUIDesc = ({ Isopen, close, pizza }) => {
                 <h3>Required</h3>
               </div>
               <div className={styles.pizzacard}>
-                {pizza.sizes.map((size) => (
-                  <div key={size.name} className={styles.sizepizza}>
+                {pizza.sizes.map((sizepizz,index) => (
+                  <div key={index} className={`${styles.sizepizza} ${selections.size===sizepizz.size ? styles.selectshow :""}` }  onClick={()=>handleToggle("size" ,sizepizz.size)}>
+                     
                     <div className={styles.img}>
-                      <img src={size.image} alt={size.name} />
+                      <img src={sizepizz.image} alt={sizepizz.name} />
                     </div>
                     <div className={styles.bdpizza}>
-                      <h5>{size.name}</h5>
-                      <p>Rs {size.price}</p>
-                      <p>({size.serves})</p>
+                      <h5>{sizepizz.size}</h5>
+                      <p>Rs {sizepizz.price}</p>
+                      <p>({sizepizz.serves})</p>
                     </div>
                   </div>
                 ))}
@@ -54,27 +79,27 @@ const ProductUIDesc = ({ Isopen, close, pizza }) => {
                 <h3>Required</h3>
               </div>
 
-              <label className={styles.pizzacard1}>
-                {pizza.pizza_crust.map((item) => (
-                  <div className={styles.sizepizza1}>
+               
+                {pizza.pizza_crust.map((item ,index) => (
+                  <label className={`${styles.pizzacard1} `}>
 
-
+                  <div key={index} className={`${styles.sizepizza1} ${selections.crust===item.title ? styles.selectionbox :""} ` } onClick={()=>handleToggle("crust" ,item.title)}>
+                <div className={`${styles.box} ${selections.crust===item.title ? styles.boxselected:""}`}  ></div>
+                  
                     <div className={styles.image1}>
                       <img src={item.image} alt="Pan Crust" />
                     </div>
-                    <input type="radio" name="crust" className={styles.radio} />
+                    <input type="radio"  checked={selections.crust===item.title} name="crust" className={styles.radio} />
                     <div className={styles.bdp}>
                       <div className={styles.bdpizza}>
                         <h5>{item.title}</h5>
                         <p>{item.description}</p>
                       </div>
                     </div>
-
-
-
                   </div>
+                  </label>
                 ))}
-              </label>
+              
 
             </div>
             <div className={styles.size}>
@@ -82,10 +107,42 @@ const ProductUIDesc = ({ Isopen, close, pizza }) => {
                 <h2><strong> More</strong><span>Add Extra Toppings</span></h2>
                 <h3>Option</h3>
               </div>
+              <div className={styles.bdy}>
+                <div className={styles.rates}>               
+                   {pizza.options.map((item,index)=>(
+                   <label key={index} htmlFor=""   >
+                  <div className={`${styles.boxes1} ${selections.optional.includes(item.title) ? styles.optionselection :""} ` } onClick={()=>handleToggle("optional", item.title)} >
+                     {item.price!==null && (
+              <div className={styles.boxes}>
+                <div  className={`${styles.box}`}>
+                  <div className={styles.select}></div>
+                  <input type="radio" name="" id="" className={styles.radio} />
+                  <h1>{item.title}</h1>
+                  </div>
+                <span>(+ Rs. {item.price})</span>
+              
+                </div>
+                 )}
+              </div>
+              </label>
+                   ))}
+               </div>
+ 
+               </div>
+            
             </div>
 
           </div>
         </div>
+        <div className={styles.instruction}  >
+               <h1 onClick={()=>handleToggle("instructions", !selections.instructions)} >kitchen Instruction(Options) <IoIosArrowDown /></h1>
+               {selections.instructions && (
+             <div className={styles.chat}>
+              <hr />
+              <textarea className={styles.textarea} name="" id=""></textarea>
+             </div>
+             )}
+          </div>
         <div className={styles.addtocart}>
           <div className={styles.btn}>
             <button>-</button>
